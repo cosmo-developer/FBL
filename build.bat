26ae66591c5772f5e7a10f43e16e1f85de73dd5e
Sonu Aryan <sonuaryan76448002@gmail.com>
2021-10-03 12:04:10 +0530

    system installer working

--- build.bat
+++ build.bat
@@ -2,8 +2,9 @@
 nasm -f bin mbr.S -o ./bin/mbr.bin
 nasm -f bin space.S -o ./bin/space.bin
 nasm -f bin kernel16.S -o ./bin/kernel16.bin
+nasm -f bin loader.S -o ./bin/loader.bin
 
-cat ./bin/mbr.bin ./bin/kernel16.bin > ./bin/bootloader.bin
+cat ./bin/mbr.bin ./bin/kernel16.bin ./bin/loader.bin > ./bin/bootloader.bin
 
 dd if=./bin/bootloader.bin of=./build/bootloader.img
 
