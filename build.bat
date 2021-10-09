3ea0c48bcc4f5c5d876113783062f4ce744c4bbd
Sonu Aryan <sonuaryan76448002@gmail.com>
2021-10-09 09:27:40 +0530

    protected mode & gdt

--- build.bat
+++ build.bat
@@ -1,11 +1,8 @@
 @echo off
 nasm -f bin mbr.S -o ./bin/mbr.bin
-nasm -f bin space.S -o ./bin/space.bin
 nasm -f bin kernel16.S -o ./bin/kernel16.bin
-nasm -f bin loader.S -o ./bin/loader.bin
-nasm -f bin graphicsKernel.S -o ./bin/graphicsKernel.bin
 
-cat ./bin/mbr.bin ./bin/graphicsKernel.bin ./bin/loader.bin > ./bin/bootloader.bin
+cat ./bin/mbr.bin ./bin/kernel16.bin > ./bin/bootloader.bin
 
 dd if=./bin/bootloader.bin of=./build/bootloader.img
 
